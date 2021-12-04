02a7607883ae2f3d60491f0707cf70511edc4b1d
carrotflakes <carrotflakes@gmail.com>
2021-12-05 00:11:32 +0900

    update bench.rs

--- examples/bench.rs
+++ examples/bench.rs
@@ -1,18 +1,39 @@
 use rand_pcg::Pcg32;
 use slidart::{Board, Solver};
 
+enum Level {
+    Easy,
+    Medium,
+    Hard,
+}
+
 fn main() {
+    let level = Level::Medium;
     let mut attempt = 0;
     let mut succeeded = 0;
     let mut total_path_length = 0;
     let mut rng = Pcg32::new(0, 0xa02bdbf7bb3c0a7);
     let time = std::time::Instant::now();
 
+    let goal = match level {
+        Level::Easy => Board::new(4, vec![0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]),
+        Level::Medium => Board::new(
+            4,
+            vec![0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1],
+        ),
+        Level::Hard => Board::new(
+            6,
+            vec![
+                0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5,
+                5, 5, 1, 1, 1, 1, 1, 1,
+            ],
+        ),
+    };
+
     for _ in 0..1000 {
-        let goal = Board::new(4, vec![0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]);
         let mut board = goal.clone();
-        board.shuffle(100, &mut rng);
-        let mut solver = Solver::new(goal, board);
+        board.shuffle(300, &mut rng);
+        let mut solver = Solver::new(goal.clone(), board);
         solver.show_progress = false;
         solver.open_node_limit = 10000;
         solver.distance_fn = Box::new(slidart::compute_distance2);
